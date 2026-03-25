9c90dbc16367018a53a0c5c5cacd0df824b7ff3d
Tanay Murade <tanaymurade8@gmail.com>
2026-03-25 18:55:55 +0530

    remove display error

--- index.js
+++ index.js
@@ -277,7 +277,7 @@ app.post("/auth/login", async (req, res) => {
 
     } catch (error) {
         console.error(error);
-        return res.status(500).json({ Message: "Error while trying to log in", Error: error });
+        return res.status(500).json({ Message: "Error while trying to log in"});
     }
 });
 
